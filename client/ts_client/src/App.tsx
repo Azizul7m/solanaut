c3815c2188113c38a2c6a92cf7d4d1ae23a4aa66
Azizul Islam <azizul7m@gmail.com>
2024-11-08 22:47:04 +0600

    Connecting browser wallet done

--- client/ts_client/src/App.tsx
+++ client/ts_client/src/App.tsx
@@ -4,14 +4,36 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { useEffect, useState } from "react";
 
+
+
 function App() {
   const {publicKey} = useWallet();
   const connection = new Connection(clusterApiUrl("devnet"));
   const [balance, setBalance] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      if(publicKey) {
+        try{
+          const balanceInLamport = await connection.getBalance(publicKey);
+          setBalance(balanceInLamport / LAMPORTS_PER_SOL);
+        } catch(error) {
+          console.error("Failed to fetch balance: ", error )
+        }
+      }
+    };
+
+    fetchBalance();
+  }, [publicKey, connection])
   return (
     <>
       <main>
         <WalletMultiButton />
+        {publicKey? (
+          <p>{balance}</p>
+        ) : (
+          <p>loading<span>😃</span></p>
+        )}
       </main>
     </>
   );
