639c0870a75f7e700ca07eb3c913c8b6cb915770
Azizul Islam <azizul7m@gmail.com>
2024-11-11 16:01:50 +0600

    sol transfer functionallity added

--- client/ts_client/src/App.tsx
+++ client/ts_client/src/App.tsx
@@ -1,64 +1,95 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import "./App.css";
 import { useWallet } from "@solana/wallet-adapter-react";
-import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { useEffect, useState } from "react";
-
-
+import {
+  clusterApiUrl,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import { FormEvent, useEffect, useState } from "react";
 
 function App() {
-  const {publicKey} = useWallet();
+  const { publicKey, sendTransaction } = useWallet();
   const connection = new Connection(clusterApiUrl("devnet"));
   const [balance, setBalance] = useState<number | null>(null);
-  const [recipient, setRecipient] = useState('');
+  const [recipient, setRecipient] = useState("");
   const [amount, setAmount] = useState<number>(0); // Amount to send
+  const [status, setStatus] = useState<string>("");
 
-  const handleSubmit = () => {
-    console.log(recipient, amount)
-  }
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (!publicKey) {
+      setStatus("Wallet not connected");
+      return;
+    }
+    try {
+      const transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: new PublicKey(recipient),
+          lamports: amount * LAMPORTS_PER_SOL,
+        }),
+      );
+      const signature = await sendTransaction(transaction, connection);
+      setStatus(`Transaction sent! Signature: ${signature}`);
+      console.log("Transaction signature: ", signature);
+    } catch (err) {
+      console.log("Transaction failed:", err);
+      setStatus("Transaction faild, See console for details");
+    }
+  };
 
   useEffect(() => {
     const fetchBalance = async () => {
-      if(publicKey) {
-        try{
+      if (publicKey) {
+        try {
           const balanceInLamport = await connection.getBalance(publicKey);
           setBalance(balanceInLamport / LAMPORTS_PER_SOL);
-        } catch(error) {
-          console.error("Failed to fetch balance: ", error )
+        } catch (error) {
+          console.error("Failed to fetch balance: ", error);
         }
       }
     };
 
     fetchBalance();
-  }, [publicKey, connection])
+  }, [publicKey, connection]);
+
+  const style = {
+    inputBox: "outline-none px-2 rounded-md",
+  };
   return (
     <>
       <main>
         <WalletMultiButton />
-        {publicKey? (
-          <p className="text-3xl font-bold underline">{balance}</p>
-        ) : (
-          <p>loading</p>
-        )}
-        <form onSubmit={handleSubmit}>
+        {publicKey ? <p>{balance}</p> : <p>loading</p>}
+        <form onSubmit={handleSubmit} className="flex gap-2">
           <input
             type="text"
             placeholder="Recipient Address"
             value={recipient}
-            onChange={(e) => setRecipient(e.target.value) }
+            onChange={(e) => setRecipient(e.target.value)}
             required
-            className="w-[100px] mt-[10px]"/>
+            className={style.inputBox}
+          />
           <input
             type="number"
             placeholder="Inter the of sol"
             value={amount}
-            onChange={e => setAmount(Number(e.target.value))}
+            onChange={(e) => setAmount(Number(e.target.value))}
             required
             min={0}
             step={0.01}
+            className={style.inputBox}
           />
-          <button type="submit">Send sol</button>
+          <button className="bg-blue-500 p-md" type="submit">
+            Send
+          </button>
         </form>
+        {status && <p className="mt-[10px]">{status}</p>}
       </main>
     </>
   );
